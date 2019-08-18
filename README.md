7a7f513ff8ab6fc494b134a4aaa05330f431e01d
ivy liu <ivy.liu.liuwanying@outlook.com>
2019-08-18 14:50:33 +0800

    1450

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # interface-framework
+* 再也不搞路径！
+---
+```python
+import os
+import sys
+base_path=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+#获取当前项目路径并添加到path中
+sys.path.append(base_path)
+
+```
--- src/run_testcase.py
+++ src/run_testcase.py
@@ -7,7 +7,10 @@ import os
 import sys
 
 # 相对路径的import
-sys.path.append("../")
+# sys.path.append("../")
+# 绝对路径的import
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../")
+# from common.utils import *
 from commons.getResponse import HttpRequestResponse
 
 class MyTest(unittest.TestCase):
